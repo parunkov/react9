38d5e07b35ed92ca7f7f2f391aae8fbe71b33304
parunkov <parunkov@gmail.com>
2023-09-04 09:31:42 +0500

    get

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Post } from './interfaces';
 
 function App() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(0);
+
+  const getPosts = () => {
+    setLoading(true);
+    axios
+        .get('https://jsonplaceholder.typicode.com/todos?_start=0&_limit=10')
+        .then((res) => {
+            setLoading(false);
+            console.log(res.data);            
+            setPosts(res.data);
+        })
+        .catch((err) => {
+            // Error handling
+            setLoading(false);
+            console.log(err);
+            return null;
+        });
+};
+useEffect(() => {
+  getPosts();
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="header"></div>
+        <div className="postsWrapper">
+          {posts.map((item) => {
+            return <div>{item.title}</div>
+          })}
+        </div>
+      </div>
     </div>
   );
 }
