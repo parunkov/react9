8fa3dafa5e203d2a6a7404251cfd66c7d46f8e12
parunkov <parunkov@gmail.com>
2023-09-04 12:16:33 +0500

    scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,64 @@
-import React, { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import axios from 'axios';
 import { Post } from './interfaces';
 import { faker } from '@faker-js/faker';
+import InfiniteScroll from 'react-infinite-scroller';
 
 function App() {
   const [posts, setPosts] = useState<Post[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [page, setPage] = useState(0);
+  const [hasMoreItems, setHasMoreItems] = useState(true);
 
   const getData = async (page: number): Promise<Post[] | null> => {
     let data: Post | null = null;
     await axios
-        .get(`https://jsonplaceholder.typicode.com/todos?_start=${page}&_limit=10`)
-        .then((res) => {
-            setLoading(false);
-            console.log(res.data); 
-            res.data.forEach((item: Post) => {
-              const string: string = faker.lorem.text();
-              item.text = string;
-              const startDate = faker.date.future({ years: 1 });
-              const endDate = faker.date.future({ years: 1, refDate: startDate });
-              item.startDate = startDate;
-              item.endDate = endDate;
-            });
-            data = res.data;           
-        })
-        .catch((err) => {
-            setLoading(false);
-            console.log(err);
-            data = null;
+      .get(`https://jsonplaceholder.typicode.com/todos?_start=${page * 10}&_limit=10`)
+      .then((res) => {
+        if (res.data.length === 0) {
+          setHasMoreItems(false);
+        } else {
+          setHasMoreItems(true);
+        }
+
+        res.data.forEach((item: Post) => {
+          const string: string = faker.lorem.text();
+          item.text = string;
+          const startDate = faker.date.future({ years: 1 });
+          const endDate = faker.date.future({ years: 1, refDate: startDate });
+          item.startDate = startDate;
+          item.endDate = endDate;
         });
+        data = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+        data = null;
+      });
+
     return data;
   }
 
-  const getPosts = async () => {
-    setLoading(true);
+  const getPosts = async (page: number) => {
     const data: Post[] | null = await getData(page);
-    if (data) setPosts(data);
-};
-useEffect(() => {
-  getPosts();
-}, []);
+    if (data) setPosts([...posts, ...data]);
+  };
 
   return (
     <div className="app">
       <div className="container">
         <div className="header"></div>
         <div className="postsWrapper">
-          {posts.map((item) => {
-            return <div key={item.id}>{item.title}</div>
-          })}
+          <InfiniteScroll
+            threshold={50}
+            pageStart={-1}
+            loadMore={getPosts}
+            hasMore={hasMoreItems}
+            loader={<div className="text-center">loading data ...</div>}>
+            {posts.map((item) => {
+              return item.id && <div className='post' key={item.id}>{item.id}</div>
+            })}
+          </InfiniteScroll>
+          {hasMoreItems ? "" : <div className="text-center">no data anymore ...</div>}
         </div>
       </div>
     </div>
